89fb3e8891d793e7bf665225b5311e3d4c55e8dc
rolguin <ext_roberto.olguin@cajalosandes.cl>
2026-03-17 18:17:15 -0400

    actualizacion de view.py

--- inventario/views.py
+++ inventario/views.py
@@ -7,10 +7,18 @@ from .models import Categoria
 def index(request):
     return HttpResponse("Hola mundo")
 
+
 def contact(request, name):
     return HttpResponse(f"Hola {name} bienvenido a la clase de Django")
 
+
 def categorias(request):
+    nombre_filtro = request.GET.get('nombre')
+    if nombre_filtro:
+        return render(request, 'categorias.html', {
+            "categorias": Categoria.objects.filter(nombre__contains=nombre_filtro)
+        })
+
     categorias = Categoria.objects.all()
     return render(request, 'categorias.html', {
         "categorias": categorias
